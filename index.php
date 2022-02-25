2f45854466d164cb27091a5cf0d11866bd566ea9
MarcoSalvatore91 <marcosalvatore91@hotmail.com>
2022-02-25 16:15:15 +0100

    added form in snack-1

--- index.php
+++ index.php
@@ -21,3 +21,89 @@ if (!empty($name) && !empty($mail) && !empty($age)) {
 }
 
 ?>
+
+<!-- Snack 2 -->
+<!-- Creare un array di array. Ogni array figlio avrà come chiave una data in questo formato: DD-MM-YYYY es 01-01-2007
+e come valore un array di post associati a quella data. 
+Stampare ogni data con i relativi post. -->
+
+<?php
+
+$posts = [
+
+    '10/01/2019' => [
+        [
+            'title' => 'Post 1',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 1'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 2'
+        ],
+    ],
+    '10/02/2019' => [
+        [
+            'title' => 'Post 3',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 3'
+        ]
+    ],
+    '15/05/2019' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 4'
+        ],
+        [
+            'title' => 'Post 5',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 5'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 6'
+        ]
+    ],
+];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snack 1</title>
+</head>
+
+<body>
+
+    <section id="Snack-1">
+        <div id="name">
+            <form>
+                <h3>Inserisci un nome</h3>
+                <input type="text" placeholder="Inserisici il tuo nome" name="name">
+
+                <h3>Inserisci un e-mail</h3>
+                <input type="text" placeholder="Inserisci la tua e-mail" name="mail">
+
+                <h3>Inserisci l'età</h3>
+                <input type="text" placeholder="Inserisci la tua età" name="age">
+
+                <button>Premi qui</button>
+            </form>
+
+        </div>
+    </section>
+
+    <section id="Snack-2">
+
+    </section>
+</body>
+
+</html>
